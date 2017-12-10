88e0e4e60f2d9c6454e7d700f13658cbca714c11
Shrikant Sonone <shrikant.sonone@coda.global>
2017-12-10 13:24:27 +0530

    [Not Working] Added mithil with model

---
+++ src/index.js
@@ -0,0 +1,4 @@
+var m = require('mithril');
+var Table = require('./table');
+console.log(Table);
+m.render(document.body, Table);
---
+++ src/table.js
@@ -0,0 +1,10 @@
+var m = require('mithril');
+
+var Table = {
+    list: [],
+    view: () => {
+        m("div", "Hello World");
+    }
+}
+
+export default Table;
