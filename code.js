1640ce010a003cc1c7c47ef980d04dd071939dda
Shrikant Sonone <shrikant.sonone@coda.global>
2017-12-10 13:58:27 +0530

    Added table and header.

--- code.js
+++ code.js
@@ -14,10 +14,43 @@ var fetchData = () => {
 }
 
 fetchData();
+const header = {
+    view: () => {
+        return m("div", [
+            [
+                m("div", [
+                    m("button", "FP"),
+                    m("button", "PP"),
+                    m("button", "PC"),
+                ]),
+                m("div", [
+                    m("div", [
+                        m("label", { for: "showing"}, "Showing"),
+                        m("input", { type: "number"})
+                    ]),
+                    m("div", [
+                        m("label", { for: "rowsout"}, "Rows Out of"),
+                        m("input", { type: "number"})
+                    ]),
+                    m("div", [
+                        m("label", { for: "starting"}, "Showing"),
+                        m("input", { type: "number"})
+                    ]),
+                ]),
+                m("div", [
+                    m("button", "LP"),
+                    m("button", "NP"),
+                    m("button", "NC"),
+                ])
+            ]
+        ]);
+    }
+}
 
 const table = {
     view: () => {
         return m("div",[
+            m("div", header),
             m("table",[
                 m("tr", [
                     m("th", { class: "ba bg-black-10" }, "id"),
@@ -40,4 +73,13 @@ const table = {
     }
 };
 
-m.mount(root, table);
+const body = {
+    view: () => {
+        return m("div",[
+            m(header),
+            m(table)
+        ])
+    }
+}
+
+m.mount(root, body);
