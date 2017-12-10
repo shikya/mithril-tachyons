8af840f831e060e2485a9f640fe72b975ce6a602
Shrikant Sonone <shrikant.sonone@coda.global>
2017-12-10 13:01:17 +0530

    Added basic table from code.

--- code.js
+++ code.js
@@ -2,8 +2,21 @@ var root = document.getElementById("mithril");
 
 const table = {
     view: () => {
-        return m("main",[
-            m("p", "Hello")
+        return m("div",[
+            m("table",[
+                m("tr", [
+                    m("th", { class: "bg-black-10" }, ""),
+                    m("th", { class: "ba bg-black-10"}, "Header"),
+                    m("th", { class: "ba bg-black-10"}, "Header"),
+                    m("th", { class: "ba bg-black-10"}, "Header"),
+                ]),
+                m("tr",[
+                    m("td", { class: "ba"}, "0"),
+                    m("td", { class: "ba"}, "Cell"),
+                    m("td", { class: "ba"}, "Cell"),
+                    m("td", { class: "ba"}, "Cell"),
+                ])
+            ])
         ]);
     }
 };
