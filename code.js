789666847fe09ebb116c2a02e7e7f07987ca0a08
Shrikant Sonone <shrikant.sonone@coda.global>
2017-12-10 15:15:40 +0530

    [FIXED] using slice instead of filter.

--- code.js
+++ code.js
@@ -2,7 +2,7 @@ var root = document.getElementById("mithril");
 var data = [];
 
 var showing = 5;
-var rowsVisible = 15;
+var rowsVisible = 100;
 var offset = 0;
 
 var fetchData = () => {
@@ -41,7 +41,15 @@ const header = {
                     ]),
                     m("div", [
                         m("label", { for: "rowsout"}, "Rows Out of"),
-                        m("input", { type: "number", value: rowsVisible })
+                        m("input", { type: "number", value: rowsVisible, oninput: (event) =>{
+                            console.log(event.srcElement.value);
+                            var value = event.srcElement.value;
+                            rowsVisible = value;
+                            if(value <= 0)
+                                rowsVisible = 1;
+                            if(value > data.length)
+                                rowsVisible = data.length
+                        } })
                     ]),
                     m("div", [
                         m("label", { for: "starting"}, "Starting"),
@@ -82,19 +90,18 @@ const table = {
                     m("th", { class: "ba bg-black-10"}, "Company"),
                     m("th", { class: "ba bg-black-10"}, "Email"),
                 ]),
-                data.filter((singleEntry, index, array) => {
-                    var dec = (index >= offset && index < (showing+offset))
-                    return dec;
-                }).map( (singleEntry, index, array) => {
-                    return m("tr",[
-                        m("td", { class: "ba"}, index ),
-                        m("td", { class: "ba"}, singleEntry.id ),
-                        m("td", { class: "ba"}, singleEntry.name ),
-                        m("td", { class: "ba"}, singleEntry.gender),
-                        m("td", { class: "ba"}, singleEntry.company),
-                        m("td", { class: "ba"}, singleEntry.email),
-                    ])
-                })
+                data.slice(offset, rowsVisible)
+                    .slice(0, showing)
+                    .map( (singleEntry, index, array) => {
+                        return m("tr",[
+                            m("td", { class: "ba"}, index ),
+                            m("td", { class: "ba"}, singleEntry.id ),
+                            m("td", { class: "ba"}, singleEntry.name ),
+                            m("td", { class: "ba"}, singleEntry.gender),
+                            m("td", { class: "ba"}, singleEntry.company),
+                            m("td", { class: "ba"}, singleEntry.email),
+                        ])
+                    })
             ])
         ]);
     }
